ca1742b0830da3d7936d22629a3b8c129fac85ce
CodeMan1993 <zhoukui@zju.edu.cn>
2019-05-25 17:24:44 +0800

    third to modify pyfiles

--- pyfiles/add.py
+++ pyfiles/add.py
@@ -2,3 +2,6 @@
 
 def add(a, b):
     return a+b
+
+def eviladd(a, b):
+    return a+b+1
--- pyfiles/helloworld.py
+++ pyfiles/helloworld.py
@@ -1,3 +1,6 @@
 # coding: utf-8
 print("Hello World")
 print("second to modify") 
+print("Hello World")
+print("third to modify") 
+
